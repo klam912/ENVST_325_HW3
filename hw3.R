a6316763a7f7bbef53f9e49dd2625071428769e8
klam912 <lamkenny912@gmail.com>
2026-02-22 21:30:45 +0000

    finished hw3. need to clean up the code and review before submission

--- hw3.R
+++ hw3.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(dplyr)
 library(lubridate)
+library(viridis)
 
 # Download datasets
 climate_change <- read.csv("./activity03/climate-change.csv")
@@ -105,3 +106,100 @@ ggplot(all_time_emissions_Americas, aes(x=Entity, y=total_emission / 1e9)) +
     y="Total CO2 Emission (billions of tons in CO2)",
     title="Total All Time Emissions for US, Mexico, and Canada"
   )
+
+# Optional challenge
+# To add a subscript in your ggplot axes labels, you need to download the ggtext
+# package. Ex: labs(x = "Subscript: CO<sub>2</sub>")
+
+
+# Homework ----
+# Question 1
+vietnam <- annual_co_region %>% filter(Entity == "Vietnam")
+
+# Graph the CO2 emission overtime in VN
+ggplot(vietnam, aes(x = Year, y = CO2 / 1e8)) +
+  geom_point() +
+  geom_line() +
+  labs(
+    x = "Year",
+    y = "Total CO2 Emission (100 million of tons in CO2)",
+    title = "Total CO2 Emission in Vietnam 1750-2020"
+  ) +
+  theme_classic()
+
+# Question 2
+world_co2data <- annual_co_region %>% filter(Entity == "World")
+world_temp_data <- climate_change %>% filter(Entity == "World")
+
+# Graph change in world air temperatures
+world_air_temp_graph <- ggplot(world_temp_data, aes(x=Day, y=temperature_anomaly)) +
+  # Blue lower half (cooler)
+  annotate("rect",
+           xmin = -Inf, xmax = Inf,
+           ymin = -Inf, ymax = 0,
+           fill = "blue", alpha = 0.1) +
+  
+  # Red upper half (warmer)
+  annotate("rect",
+           xmin = -Inf, xmax = Inf,
+           ymin = 0, ymax = Inf,
+           fill = "red", alpha = 0.1) +
+  geom_line() +
+  geom_hline(yintercept = 0, color="red") +
+  labs(
+    x = "Year",
+    y = "Temperature Anomaly (Celsius)",
+    title = "Rising World Temperature Anomaly (1880-2021)",
+    caption = "Note: Temperature anomaly shows how much warmer or cooler a year is compared to the long-term average."
+  ) +
+  theme(
+    plot.caption = element_text(size = 9, hjust = 0) # move the caption to be left-aligned with font 9
+  )
+
+# Graph change in CO2 emission for world
+world_co2data1 <- world_co2data %>%
+  arrange(Year) %>%
+  mutate(co2_emission_diff = CO2 - lag(CO2)) # subtract CO2 from current to past year
+
+world_co2_emission_graph <- ggplot(world_co2data1, aes(x=Year, y=co2_emission_diff/1e9)) +
+  geom_line() +
+  labs(
+    x = "Year",
+    y = "Change in CO2 Emission (billion of tons in CO2)",
+    title = "Changes in CO2 Emission (1750-2020)"
+  ) +
+  geom_hline(yintercept = 0, color="red")
+
+# Question 3
+# Read in data
+carbon_footprint_data <- read.csv("carbon-footprint-travel-mode.csv")
+
+# Remake the graph
+ggplot(carbon_footprint_data,
+       aes(x=Transport.emissions.per.kilometer.travelled, y=reorder(Entity, Transport.emissions.per.kilometer.travelled), # sort by carbon footprint
+           fill=Entity)) +
+  geom_bar(stat="identity") +
+  geom_text(aes(label = paste0(round(Transport.emissions.per.kilometer.travelled, 0), "g")), # customize the bar label
+            hjust = -0.1) +  # labels slightly to the right of bars
+  scale_fill_viridis_d(option = "plasma") + # discrete viridis palette (another library with color-blindness palatte)
+  labs(
+    x = NULL, # remove axes labels
+    y = NULL,
+    title = "Carbon footprint of travel per kilometer, 2022",
+    subtitle = "The carbon footprint of travel is measured in grams of carbon dioxide-equivalents per passenger kilometer. This includes the
+impact of increased warming from aviation emissions at altitude.",
+    caption = "Data source: UK Government, Department for Energy Security and Net Zero (2022) – Learn more about this data
+
+Note: Official conversion factors used in UK reporting. These factors will vary across countries depending on energy mix, transport
+technologies, and occupancy of public transport. Data for aviation is based on economy class.
+
+OurWorldinData.org/transport | CC BY"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = "none", # remove legend
+    axis.text.x = element_blank(),   # remove x-axis numbers
+    axis.ticks.x = element_blank(),   # remove x-axis ticks if you want
+    plot.caption = element_text(size = 9, hjust = 0),
+    panel.grid = element_blank() # remove gridlines
+    )
